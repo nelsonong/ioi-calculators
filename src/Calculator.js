2962104062733502ce4d0ac87fd53adfc67b1151
nelsonong <nelsonong3@gmail.com>
2018-06-20 10:41:11 -0400

    Add change handler for Option component

--- src/Calculator.js
+++ src/Calculator.js
@@ -13,11 +13,14 @@ class Calculator extends Component {
         this.state = {
             link: 'cl',                                     // Link type (Camera Link, CoaXPress)
             model: '2M360MCL/NCL',                          // Camera model
-            hwversion: '2',                                   // Hardware version
+            hwversion: '2',                                 // Hardware version
             format: 'Base 8-bit x 2',                       // Camera link format
             bitDepth: 8, linkCount: 1, linkSpeed: 'CXP-3',  // CoaXpress format
             width: 1920,                                    // Resolution - width
-            height: 1080                                    // Resolution - height
+            height: 1080,                                   // Resolution - height
+            subSampling: false,
+            slowMode: false
+
         };
 
         this.updateState = this.updateState.bind(this);
@@ -34,11 +37,11 @@ class Calculator extends Component {
     render() {
         return (
             <div className="Calculator">
-                <Model updateLink={this.updateLink} updateState={this.updateState}/>
+                <Model updateState={this.updateState}/>
                 <HardwareVersion updateState={this.updateState}/>
                 <Format link={this.state.link} model={this.state.model} updateState={this.updateState}/>
                 <Resolution updateState={this.updateState} width={this.state.width} height={this.state.height}/>
-                <Options updateState={this.updateState}/>
+                <Options link={this.state.link} model={this.state.model} format={this.state.format} updateState={this.updateState}/>
                 <Capabilities updateState={this.updateState}/>
             </div>
         );
--- src/Options.js
+++ src/Options.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 
 class Options extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            supportedFormats: [ 'Full 8-bit x 8', '80-bit 10-bit x 8' ]
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    subSamplingDisabled() {
+        const currentModel = this.props.model;
+        return (currentModel.startsWith('12M') || currentModel.startsWith('48M'));
+    }
+
+    slowModeDisabled() {
+        const link = this.props.link;
+        const model = this.props.model;
+        const format = this.props.format;
+
+        return (link !== 'cl' || model.startsWith('12M') || !this.state.supportedFormats.includes(format));
+    }
+
+    handleInputChange(e) {
+        const name = e.target.name;
+        const checked = e.target.checked;
+
+        this.props.updateState({ [name]: checked });
+    }
+
     render() {
         return (
             <fieldset>
             <legend>Options</legend>
-                <input type="checkbox" name="hv" value="1" />Enable sub-sampling
+                <input type="checkbox" name='subSampling' disabled={this.subSamplingDisabled()} onChange={this.handleInputChange}/>Enable sub-sampling
                 <br />
-                <input type="checkbox" name="hv" value="2" />Enabled reduced line rate mode
+                <input type="checkbox" name='slowMode' disabled={this.slowModeDisabled()} onChange={this.handleInputChange}/>Enabled reduced line rate mode
             </fieldset>
         );
     }
