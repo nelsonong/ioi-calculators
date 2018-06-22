8cf7044d7d2c4a24032988a2bac41191e8dbe27c
nelsonong <nelsonong3@gmail.com>
2018-06-22 16:56:50 -0400

    Add button to create FlareCalculator components

--- src/App.js
+++ src/App.js
@@ -3,18 +3,33 @@ import FlareCalculator from './containers/FlareCalculator';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-        <div className="App">
-            <h3>
-                Flare Frame Rate Calculator
-            </h3>
-            <FlareCalculator />
-            <FlareCalculator />
-            <FlareCalculator />
-        </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            flareCalculators: [ <FlareCalculator /> ]
+        };
+
+        this.addFlare = this.addFlare.bind(this);
+    }
+
+    addFlare() {
+        const flareCalculators = this.state.flareCalculators;
+        this.setState({ flareCalculators: flareCalculators.concat(<FlareCalculator />) });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h3>
+                    Flare Frame Rate Calculator
+                </h3>
+                <div className='AddButtons'>
+                    <button type='button' onClick={this.addFlare}>Add Flare Calculator</button>
+                </div>
+                {this.state.flareCalculators}
+            </div>
+        );
+    }
 }
 
 export default App;
