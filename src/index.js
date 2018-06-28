fcecc6fa5aef56468911020cce893bfaeaa37fd0
nelsonong <nelsonong3@gmail.com>
2018-06-28 00:58:05 -0400

    Set up webpack with babel and css loaders for development

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import FrameRate from './scenes/FrameRate/FrameRate';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<FrameRate />, document.getElementById('root'));
-registerServiceWorker();
