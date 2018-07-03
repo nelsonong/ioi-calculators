9f8617150ae3945b481ce2465353508ec59bf4b8
nelsonong <nelsonong3@gmail.com>
2018-07-03 11:13:49 -0400

    Fix incorrect imports

--- src/screens/Storage/Storage.js
+++ src/screens/Storage/Storage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'React';
+import React, { Component } from 'react';
 import DVRCalculator from '../../components/DVRCalculator';
 import InstructionBox from '../../components/InstructionBox';
 import './Storage.css';
