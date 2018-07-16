7db9a8daf87f7b1538f608db1ce2d0f121a184af
nelsonong <nelsonong3@gmail.com>
2018-07-16 10:33:11 -0400

    Fix number of links associated with 4KSDI interfaces

--- webpack.config.js
+++ webpack.config.js
@@ -63,7 +63,8 @@ module.exports = (env) => {
         devServer: {
             contentBase: path.join(__dirname, 'public'),
             historyApiFallback: true,
-            publicPath: 'dist'
+            publicPath: '/dist/',
+            port: 8080
         }
     };
 };
