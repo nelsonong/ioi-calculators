a12c3ba76e6503c66b87610b11f99111797e93ae
nelsonong <nelsonong3@gmail.com>
2018-07-17 08:14:26 -0400

    Install redux and react-redux

--- webpack.config.js
+++ webpack.config.js
@@ -63,7 +63,7 @@ module.exports = (env) => {
         devServer: {
             contentBase: path.join(__dirname, 'public'),
             historyApiFallback: true,
-            publicPath: 'dist'
+            publicPath: '/dist/'
         }
     };
 };
