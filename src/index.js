7ad93947ac9afa1fff5436bda7ad438284cba631
nelsonong <nelsonong3@gmail.com>
2018-07-20 09:17:37 -0400

    Set up redux dev tools and configure to work with Maps

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import 'map.prototype.tojson';
 import configureStore from './store/configureStore';
 import AppRouter from './routers/AppRouter';
 import 'normalize-css/normalize.css';
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,9 +2,12 @@ import { createStore, combineReducers } from 'redux';
 import frameRateReducer from '../reducers/frameRateReducer';
 import storageReducer from '../reducers/storageReducer';
 
-const configureStore = () => createStore(combineReducers({
-    frameRateCalculators: frameRateReducer,
-    storageCalculators: storageReducer
-}));
+const configureStore = () => createStore(
+    combineReducers({
+        frameRateCalculators: frameRateReducer,
+        storageCalculators: storageReducer
+    }),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default configureStore;
