c0adf67f64385e4b74df0d9a78419383fd61f687
nelsonong <nelsonong3@gmail.com>
2018-07-27 08:52:02 -0400

    Fix resolution constants to match manual's

--- src/components/FlareSDICalculator/constants/resolutions.js
+++ src/components/FlareSDICalculator/constants/resolutions.js
@@ -1,8 +1,8 @@
 export default {
-  DCI_4K: '4K (4096x2160)',
-  UHD: 'UHD (3840x2160)',
-  DCI_2K: '2K (2048x1080)',
-  FHD: 'FHD (1920x1080)',
-  FHD_I: 'FHDi (1920x1080)',
-  HD: 'HD (1280x720)',
+  DCI_4K: '4K (2160p)',
+  UHD: 'UHD (2160p)',
+  DCI_2K: '2K (1080p)',
+  FHD: 'HD (1080p)',
+  FHD_I: 'HD (1080i)',
+  HD: 'HD (720p)',
 };
