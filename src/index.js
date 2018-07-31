9d2a58e9e6d00ccef4ee20873614f51298250cd7
nelsonong <nelsonong3@gmail.com>
2018-07-31 15:27:11 -0400

    Remove map.prototype.json and revert to ESLint 4.x

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import 'map.prototype.tojson';
 import configureStore from './store/configureStore';
 import AppRouter from './routers/AppRouter';
 import 'normalize-css/normalize.css';
