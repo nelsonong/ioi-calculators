1551bb175f87b0ca14020df08aeda3260698d38e
nelsonong <nelsonong3@gmail.com>
2018-08-27 16:47:09 -0400

    Update version to 1.01

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -11,7 +11,7 @@ import {
 import cameraReducer from '../reducers/cameraReducer';
 import storageReducer from '../reducers/storageReducer';
 
-const version = '1.00';
+const version = '1.01';
 const persistedVersion = loadVersion();
 let persistedState = loadState();
 
